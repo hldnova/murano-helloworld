50d6c3f9d85971e4eb79c0ab9f9c21c7edd839e5
Lida He <lida.he@dell.com>
2016-10-14 20:28:50 -0400

    removed a couple of open ports that are not needed

--- start-murano.sh
+++ start-murano.sh
@@ -13,9 +13,8 @@ MURANO_CONTAINER_IP=10.247.134.56
 
 OS_PROTOCOL=http
 OS_USERNAME=admin 
-OS_PASSWORD=admin123
+OS_PASSWORD=password
 OS_PROJECT_NAME=admin 
-OS_DOMAIN_NAME=default 
 
 LOCAL_MURANO_PORT=8082
 LOCAL_HORIZON_PORT=8000
@@ -30,7 +29,6 @@ LOG_DIR=/tmp/murano/logs
 docker rm -f $CONTAINER_NAME
 docker run -d -v $LOG_DIR:/logs \
     -p ${LOCAL_MURANO_PORT}:8082 -p ${LOCAL_HORIZON_PORT}:8000 -p ${LOCAL_RABBIT_PORT}:5672 \
-    -p 15672:15672 -p 55672:55672 \
     -h `hostname` \
     -e OS_PROTOCOL=$OS_PROTOCOL \
     -e OS_KEYSTONE_HOST=$KEYSTONE_HOST \
@@ -39,7 +37,6 @@ docker run -d -v $LOG_DIR:/logs \
     -e OS_USERNAME_TEMP=$OS_USERNAME \
     -e OS_PASSWORD_TEMP=$OS_PASSWORD \
     -e OS_PROJECT_NAME_TEMP=$OS_PROJECT_NAME \
-    -e OS_DOMAIN_NAME_TEMP=$OS_DOMAIN_NAME \
     -e OS_RABBIT_USERID=$OS_RABBIT_USERID \
     -e OS_RABBIT_PASSWORD=$OS_RABBIT_PASSWORD \
     -e OS_MURANO_API_PORT=$LOCAL_MURANO_PORT \
@@ -50,4 +47,4 @@ docker ps | grep $CONTAINER_NAME
 echo 
 echo "To monitor the progress: tail -f $LOG_DIR/murano-init.log"
 echo "More logs at $LOG_DIR" 
-echo "Connect to http://${MURANO_CONTAINER_IP}:${LOCAL_HORIZON_PORT}"
+echo "Murano dashboard: http://${MURANO_CONTAINER_IP}:${LOCAL_HORIZON_PORT}"
